348ae0def248de09e5666a40d5b0c3fa14f7e0c4
Eric Shu <eshu2146@gmail.com>
2023-02-01 10:06:26 -0800

    Add Setting Up Flask file

---
+++ Setting up Flask/Setting_Up_Flask.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('helloworld.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port = '8080', debug = 'true')
