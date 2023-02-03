2dc1ecaa624b954b88b053ccd717d35b4d335edc
Eric <eshu2146@gmail.com>
2023-02-02 20:02:46 -0800

    Feb 2, 2023 Update

--- Setting up Flask/Setting_Up_Flask.py
+++ Setting up Flask/Setting_Up_Flask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('helloworld.html')
+    return render_template('helloworld.html', value = "Sunny")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = '8080', debug = 'true')
