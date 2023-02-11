9ef89a0852f22c1e17b3d38265f680517944c66e
Eric Shu <eshu2146@gmail.com>
2023-02-11 09:39:34 -0800

    Add 7.12.2 Version

---
+++ Feb 11, 2023 - V7.12.2 Solar Panel Autonomy/V1.12.2_Ard-Py_Comm/V1.12.2_Ard-Py_Comm.ino
@@ -0,0 +1,129 @@
+/*
+  Main file for the communication between our Arduino to Raspbery Pi
+  Rev 1.12.2 Changes
+  - Added autonomous solar panel movement
+  (Current Rev)
+  (Future Rev)
+  - Review IMU calibration
+    - Check the bounds of the IMU (240/290/270/290)
+    - Determine what the compass heading points toward (what degree is N/E/S/W)
+  - When the Rover lost comms, flag it and find a safe spot
+  - Code SOC check for power percent
+  - When rover is stopped for charging, code the solar panel power check to work alongside with the rover turning
+*/
+
+#include "Movement.h"
+
+// String variables for the serial message
+String nom = "Solar Turtle is ";
+String message = "";
+String command;
+String tempString; 
+int spaceIndex;
+int num;
+bool initFlag = true;
+
+int initPanelIterator = 0;
+
+// Start serial on Arduino power up
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read the command from the serial port
+  readSerialPort();
+  message.toLowerCase();   
+
+  // Initialization check
+  if (initFlag == true) {
+    initPanelIterator = 0;
+    initMovement();
+    initData();
+    initFlag = false;
+  }
+  else {
+    // Autonomous solar panel (Checks while the robot is idle/waiting for command)
+    if (message == "") {
+      // Panel Iterator will allow us to delay how often the solar panel runs automation
+      if (initPanelIterator > 20) {
+        initPanelIterator = 0;
+        AutonomousSolarPanel();
+      }
+      else {
+        ++initPanelIterator;
+      }
+    }
+    else {
+    // Stop command stops the robots movement
+      if (message == "init") {
+        Serial.print(nom + "reinitializing!");
+        initFlag = true;
+      }
+      else if (message == "quick stop") {
+        Serial.print(nom + "Quick Stopping...");
+        Stop(3);
+      }
+      else if (message == "stop") {
+        Serial.print(nom + "Stopping...");
+        Stop(2);
+      }
+      else if (message == "slow stop") {
+        Serial.print(nom + "Slow Stopping...");
+        Stop(1);
+      }
+      // Data command sends important values such as GPS coordinates or current Roll/Pitch/Yaw
+      else if (message == "data") {
+        sendData();
+      } 
+      // If the command is none of the above, it will contain a number, space, then the command (XXX COMMAND)
+      else {
+        // Find the index of the space (spaceIndex) and split message into two strings, number and command
+        spaceIndex = message.indexOf(" ");
+        tempString = message.substring(0, spaceIndex);
+        num = tempString.toInt();
+        command = message.substring((spaceIndex + 1), message.length());
+        
+        if (command == "forward") {
+          Serial.print(nom + "Schmooving Forward at " + String(num) + "% speed");
+          MoveForward(num);    
+        } 
+        else if (command == "reverse") {
+          Serial.print(nom + "Reverse Reverse at " + String(num) + "% speed");
+          MoveReverse(num);
+        }
+        else if (command == "left") {
+          Serial.print(nom + "Turning to the Left " + String(num) + " degrees");
+          TurnLeft(num);
+        }
+        else if (command == "right") {
+          Serial.print(nom + "Turning to the Right " + String(num) + " degrees");
+          TurnRight(num);
+        }
+        else if (command == "panel") {
+          Serial.print("Schmooving the Panel to " + String(num) + " degrees");
+          MovePanel(num);
+        }
+        else if (command == "error") {
+          Serial.print("New error margin of " + String(num) + "applied, I hope you know what you're doing...");
+          changeErrorMargin(num);
+        }
+        num = 0;
+        command = "";
+      }
+    }
+  }
+  delay(250);
+}
+
+// Read serial port
+void readSerialPort() {
+  message = "";
+    if (Serial.available()) {
+      delay(10);
+      while (Serial.available() > 0) {
+        message += (char)Serial.read();
+      }
+      Serial.flush();
+    }
+}
