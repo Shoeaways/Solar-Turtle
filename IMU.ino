3b2acab657b3b9f765386d56b4f51d65806e80b9
Eric Shu <eshu2146@gmail.com>
2023-02-13 09:24:18 -0800

    New IMU code usng quaternions

--- IMU.ino
+++ IMU.ino
@@ -1,68 +1,73 @@
-#include "MPU9250.h"
+#include <MPU9250.h>
+#include <Wire.h>
+#include <math.h>
 
-// Setup IMU connection
-MPU9250 IMU(Wire,0x68);
-// IMU variables
-int status;
-float accelX, accelY, accelZ;
-float gyroX, gyroY, gyroZ;
-float magX, magY, magZ;
-float pitch, roll, yaw;
-float Yh, Xh;
+MPU9250 mpu;
 
+#define Kp 0.5 // Proportional gain
+#define Ki 0.0 // Integral gain
+
+float q0 = 1.0, q1 = 0.0, q2 = 0.0, q3 = 0.0; // Quaternion elements representing the estimated orientation
+float exInt = 0.0, eyInt = 0.0, ezInt = 0.0; // Integral error terms for the complementary filter
+float yawEst = 0.0; // Estimated yaw angle (in degrees)
 
 void setup() {
-  // Initialize IMU
-Serial.begin(9600);
-status = IMU.begin();
+  Wire.begin();
+  Serial.begin(9600);
 
-// setting the accelerometer full scale range to +/-2G 
-IMU.setAccelRange(MPU9250::ACCEL_RANGE_2G);
-// setting the gyroscope full scale range to +/-250 deg/s
-IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
-// setting DLPF bandwidth to 5 Hz
-IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_5HZ);
-// setting SRD to 19 for a 50 Hz update rate
-IMU.setSrd(19); 
-  // put your setup code here, to run once:
-
+  mpu.setup();
+  mpu.calibrateGyro();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-// Read values from IMU and GPS sensors
-IMU.readSensor();
+  mpu.update();
 
-// Fill variables with sensor values
-// IMU Values
-accelX = IMU.getAccelX_mss();
-accelY = IMU.getAccelY_mss();
-accelZ = IMU.getAccelZ_mss();
-gyroX = IMU.getGyroX_rads();
-gyroY = IMU.getGyroY_rads();
-gyroZ = IMU.getGyroZ_rads();
-magX = IMU.getMagX_uT();
-magY = IMU.getMagY_uT();
-magZ = IMU.getMagZ_uT();
-// // GPS Values
-// Latitude = gps.location.lat();
-// Longitude = gps.location.lng();
-// roverSpeed = gps.speed.mps(); 
+  float ax = mpu.getAccelX();
+  float ay = mpu.getAccelY();
+  float az = mpu.getAccelZ();
+  float gx = mpu.getGyroX();
+  float gy = mpu.getGyroY();
+  float gz = mpu.getGyroZ();
 
-// Calculate roll, pitch, and yaw
-roll = atan2(-accelX,(sqrt((accelY * accelY) + (accelZ * accelZ))));
-pitch = atan2 (accelY,(sqrt ((accelX * accelX) + (accelZ * accelZ))));
-Yh = (magY * cos(roll)) - (magZ * sin(roll));
-Xh = (magX * cos(pitch))+(magY * sin(roll)*sin(pitch)) + (magZ * cos(roll) * sin(pitch));
-yaw =  atan2(Yh, Xh);
+  // Convert accelerometer readings to pitch and roll angles
+  float pitch = atan2(-ax, sqrt(ay*ay + az*az));
+  float roll = atan2(ay, az);
 
-// Convert from Radians to Degrees
-roll *= 180/(PI);
-pitch *= 180/(PI);
-yaw *= 180/(PI);
+  // Compute complementary filter
+  float dt = mpu.getDeltaT() / 1000.0;
+  float gyroScale = 0.0175; // Gyro scale factor (degrees per second)
+  float qDot1 = 0.5 * (-q1 * gx - q2 * gy - q3 * gz);
+  float qDot2 = 0.5 * (q0 * gx + q2 * gz - q3 * gy);
+  float qDot3 = 0.5 * (q0 * gy - q1 * gz + q3 * gx);
+  float qDot4 = 0.5 * (q0 * gz + q1 * gy - q2 * gx);
+  exInt += qDot1 * dt;
+  eyInt += qDot2 * dt;
+  ezInt += qDot3 * dt;
+  q0 += qDot4 * dt;
+  q1 -= qDot1 * dt;
+  q2 -= qDot2 * dt;
+  q3 -= qDot3 * dt;
 
-// Alter range to be 0 to 360 instead of -180 to 180
-yaw += 180;
-Serial.println(yaw);
-delay(200);
+  // Normalize quaternion
+  float norm = sqrt(q0*q0 + q1*q1 + q2*q2 + q3*q3);
+  q0 /= norm;
+  q1 /= norm;
+  q2 /= norm;
+  q3 /= norm;
+
+  // Use complementary filter to combine accelerometer and gyro readings
+  float pitchEst = q0*q0*pitch - 2*q0*q2*roll + 2*q0*q3*gyroScale*gy;
+  float rollEst = q0*q0*roll + 2*q0*q1*gyroScale*gx + 2*q0*q2*pitch;
+  pitchEst = (pitchEst * Kp + exInt * Ki) * 180.0 / M_PI;
+  rollEst = (rollEst * Kp + eyInt * Ki) * 180.0 / M_PI;
+
+  // Compute estimated yaw angle
+  float yawAccel = atan2(ay, ax) * 180.0 / M_PI; // Yaw angle based on accelerometer readings
+  yawEst = 0.98 * (yawEst + gyroScale * gz * dt) + 0.02 * yawAccel; // Complementary filter to combine accelerometer and gyro
+
+  Serial.println(rollEst);
+  Serial.println(pitchEst);
+  Serial.println(yawEst);
+
+  delay(250);
 }
